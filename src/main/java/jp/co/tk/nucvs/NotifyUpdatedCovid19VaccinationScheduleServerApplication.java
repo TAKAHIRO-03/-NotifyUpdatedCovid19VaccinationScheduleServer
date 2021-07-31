3bcc14b1e971e9093caf1d7b6e11965c4e6df36c
TAKAHIRO-03 <1701010402qb@cyber-u.ac.jp>
2021-07-31 17:50:06 +0900

    first commit

---
+++ src/main/java/jp/co/tk/nucvs/NotifyUpdatedCovid19VaccinationScheduleServerApplication.java
@@ -0,0 +1,13 @@
+package jp.co.tk.nucvs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotifyUpdatedCovid19VaccinationScheduleServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotifyUpdatedCovid19VaccinationScheduleServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/jp/co/tk/nucvs/NotifyUpdatedCovid19VaccinationScheduleServerApplicationTests.java
@@ -0,0 +1,13 @@
+package jp.co.tk.nucvs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotifyUpdatedCovid19VaccinationScheduleServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
