2293b1277866cd8ccf80fc6fec0612d5e5444722
TAKAHIRO-03 <1701010402qb@cyber-u.ac.jp>
2021-08-13 02:09:45 +0900

    SQL文修正

--- src/main/java/jp/co/tk/nucvs/domain/repo/Covid19VaccinationScheduleRepository.java
+++ src/main/java/jp/co/tk/nucvs/domain/repo/Covid19VaccinationScheduleRepository.java
@@ -11,13 +11,10 @@ import jp.co.tk.nucvs.domain.model.Covid19VaccinationSchedule;
 @Repository
 public interface Covid19VaccinationScheduleRepository extends JpaRepository<Covid19VaccinationSchedule, Integer> {
 
-    @Override
-    @Query(value = "SELECT count(c.id) FROM Covid19VaccinationSchedule c")
-    public long count();
-
     @Query(value = "SELECT cs FROM Covid19VaccinationSchedule cs INNER JOIN FETCH cs.covid19VaccinationVenue cv WHERE cv.city = :city ORDER BY cs.availabilityDate, cs.availabilityCount")
     public List<Covid19VaccinationSchedule> findByCovid19vsOrderByAvaDateAndAvaCnt(String city);
 
+    @Query(value = "SELECT cs FROM Covid19VaccinationSchedule cs INNER JOIN FETCH cs.covid19VaccinationVenue cv ORDER BY cs.availabilityDate, cs.availabilityCount")
     public List<Covid19VaccinationSchedule> findAllByOrderByAvailabilityDateAscAvailabilityCountAsc();
 
 }
